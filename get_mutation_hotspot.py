284fa64d0f615981f8d44ea2acf582581c55c85e
Ying Li <59164291+liying0128@users.noreply.github.com>
2021-10-16 14:38:33 +0800

    Add files via upload

--- get_mutation_hotspot.py
+++ get_mutation_hotspot.py
@@ -2,8 +2,14 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 import pandas as pd
 import matplotlib.pyplot as plt
-def getmutationhotspot(protein_length,file,protein_num):
-    b=[]
+def getmutationhotspot(file):
+    similarity=[]
+    Protein_length=[]
+    paralog_num=0
+    for i in SeqIO.parse(file,'fasta'):
+        paralog_num=paralog_num+1
+        Protein_length.append(len(i))
+    protein_length=max(Protein_length)
     for i in range(protein_length):
         a=[]
         for seq_record in SeqIO.parse(file,'fasta'):
@@ -15,6 +21,7 @@ def getmutationhotspot(protein_length,file,protein_num):
             else:
                 a.append(seq[i])
         result=pd.value_counts(a)
-        b.append(result[0]/protein_num)
-    plt.plot(range(protein_length),b)
-        
+        similarity.append((1-result[0]/paralog_num)*100)
+    plt.plot(range(protein_length),similarity,color='red')
+    plt.ylabel('Mutation rate (%)',fontsize=12)
+    plt.xlabel('Position',fontsize=12) 
